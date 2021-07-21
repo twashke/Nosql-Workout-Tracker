2f2fd5f3d375ed4e136060bf802b0610a86d1350
Tiffany Washke <twashke@gmail.com>
2021-07-21 14:37:59 -0700

    updated comments

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,11 +6,11 @@ module.exports = (app) => {
     app.get("/", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/html/index.html"));
     })
-    // Get the exercise page
+    // Get the page to enter the excercise
     app.get("/exercise", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/html/exercise.html"));
     })
-    // Get the workout page
+    // Get the stats page
     app.get("/stats", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/html/stats.html"));
     })
