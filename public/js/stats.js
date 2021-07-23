230cebe211ba46af07cf3edfa824611bd996ed36
Tiffany Washke <twashke@gmail.com>
2021-07-23 12:51:38 -0700

    updated file to add functions to find cardio distance and display a bar chart

--- public/js/stats.js
+++ public/js/stats.js
@@ -15,12 +15,33 @@ function calculateTotalWeight(data) {
   return totals;
 }
 
+// Added function to Calculate Cardio Distance
+function calculateTotalDistance(data) {
+  const cardioTotals = [];
+
+  data.forEach((workout) => {
+    const cardioTotal = workout.exercises.reduce((total, { type, distance }) => {
+      if(type === "cardio") {
+        return total + distance;
+      }
+      return total;
+    }, 0);
+    cardioTotals.push(cardioTotal);
+  });
+
+  return cardioTotals;
+}
+
 function populateChart(data) {
   const durations = data.map(({ totalDuration }) => totalDuration);
   const pounds = calculateTotalWeight(data);
+  // Added distance for cardio
+  const distance = calculateTotalDistance(data);
+  console.log(distance);
 
   const line = document.querySelector('#canvas').getContext('2d');
   const bar = document.querySelector('#canvas2').getContext('2d');
+  const bar2 = document.querySelector('#canvas3').getContext('2d');
 
   const labels = data.map(({ day }) => {
     const date = new Date(day);
@@ -105,7 +126,55 @@ function populateChart(data) {
       },
     },
   });
+
+  // Added bar chart for cardio distance
+  let barChart2 = new Chart(bar2, {
+    type: 'bar',
+    data: {
+      labels,
+      datasets: [
+        {
+          label: 'Distance',
+          data: distance,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+          ],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Distance traveled (Last 7 days)',
+      },
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+        ],
+      },
+    },
+  });
 }
+
+
 
 // get all workout data from back-end
 API.getWorkoutsInRange().then(populateChart);
