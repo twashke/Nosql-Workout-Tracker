3119fd75c269d1eb399ced8c0cad5cea1968dcd3
Tiffany Washke <twashke@gmail.com>
2021-08-04 10:40:37 -0700

    seed database

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
-// require("./seeders/seed");
+require("./seeders/seed");
 
 // Declare Port
 const PORT = process.env.PORT || 3000;
